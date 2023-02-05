c1f2a2f708946fcc03629da25023cf3a41d6a346
Quoc PHAN <phquoc25@gmail.com>
2023-02-05 14:59:29 +0700

    [add-jasmine-html-reporter] adding reporter to show the test result in the debug html page

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://www.npmjs.com/search?q=keywords:karma-reporter
-    reporters: ['progress', 'coverage'],
+    reporters: ['kjhtml', 'progress', 'coverage'],
 
      // optionally, configure the reporter
     coverageReporter: {
