01a35464952ce85e9c719328cd92fbbe7e782d27
Quoc PHAN <phquoc25@gmail.com>
2023-02-05 22:21:44 +0700

    [add-jasmine-html-reporter] keep test file next to the js file

--- karma.conf.js
+++ karma.conf.js
@@ -13,10 +13,19 @@ module.exports = function(config) {
     frameworks: ['jasmine'],
 
 
+    client: {
+      jasmine: {
+          // you can add configuration options for Jasmine here
+          // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+          // for example, you can disable the random execution with `random: false`
+          // or set a specific seed with `seed: 4321`
+      },
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+    },
+  
     // list of files / patterns to load in the browser
     files: [
-      'src/**/*.js',
-      'test/**/*.js'
+      'src/**/*.js'
     ],
 
 
@@ -31,7 +40,7 @@ module.exports = function(config) {
       // source files, that you wanna generate coverage for
       // do not include tests or libraries
       // (these files will be instrumented by Istanbul)
-      'src/**/*.js': ['coverage']
+      'src/**/!(*.spec)*.js': ['coverage']
     },
 
 
